234e372fb4e9a792b61fd700eb872712209d4806
Franck Magnan <fmagnan@gmail.com>
2013-02-21 12:36:26 +0100

    modify preformatted format as option

--- lib/SqlFormatter.php
+++ lib/SqlFormatter.php
@@ -316,7 +316,7 @@ class SqlFormatter
      *
      * @return String The SQL string with HTML styles and formatting wrapped in a <pre> tag
      */
-    public static function format($string, $highlight=true) {
+    public static function format($string, $highlight=true,$preformatted=true) {
         // This variable will be populated with formatted html
         $return = '';
 
@@ -531,7 +531,7 @@ class SqlFormatter
         $return = trim(str_replace("\t",self::$tab,$return));
 
         if ($highlight) {
-            $return = "<pre ".self::$pre_attributes.">" . $return . "</pre>";
+            $return = self::output($return,$preformatted);
         }
 
         return $return;       
@@ -544,7 +544,7 @@ class SqlFormatter
      *
      * @return String The SQL string with HTML styles applied
      */
-    public static function highlight($string)
+    public static function highlight($string,$preformatted=true)
     {
         $tokens = self::tokenize($string);
 
@@ -554,7 +554,7 @@ class SqlFormatter
             $return .= self::highlightToken($token);
         }
 
-        return "<pre ".self::$pre_attributes.">" . trim($return) . "</pre>";
+        return self::output($return,$preformatted);
     }
 
     /**
@@ -783,5 +783,16 @@ class SqlFormatter
      */
     private static function quote_regex($a) {
         return preg_quote($a,'/');
+    }
+    
+    private static function output($string,$preformatted=true){
+        $string=trim($string);
+        if($preformatted){
+            return '<pre '.self::$pre_attributes.'>' . $string . '</pre>';
+        }
+        else {
+            return $string;
+        }
+        
     }
 }
