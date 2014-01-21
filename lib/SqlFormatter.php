5e3938b7125cd677d8e4e0a4ac2630524bb43df1
Dominik Liebler <liebler.dominik@googlemail.com>
2014-01-21 12:28:01 +0100

    Prevent warnings in highlightToken

--- lib/SqlFormatter.php
+++ lib/SqlFormatter.php
@@ -864,7 +864,7 @@ class SqlFormatter
         if (self::is_cli()) {
             $token = $token[self::TOKEN_VALUE];
         } else {
-            $token = htmlentities($token[self::TOKEN_VALUE],ENT_COMPAT,'UTF-8');
+            $token = @htmlentities($token[self::TOKEN_VALUE],ENT_COMPAT,'UTF-8');
         }
 
         if ($type===self::TOKEN_TYPE_BOUNDARY) {
