746b38e0e6599256998b546053a633348644a5dd
Michal Jurečko <michal.jurecko@gmail.com>
2020-12-11 10:33:12 +0100

    Generalization of comment implementation

--- lib/SqlFormatter.php
+++ lib/SqlFormatter.php
@@ -39,7 +39,7 @@ class SqlFormatter
         'CHARSET', 'CHECK', 'CHECKSUM', 'COLLATE', 'COLLATION', 'COLUMN', 'COLUMNS', 'COMMENT', 'COMMIT', 'COMMITTED', 'COMPRESSED', 'CONCURRENT',
         'CONSTRAINT', 'CONTAINS', 'CONVERT', 'CREATE', 'CROSS', 'CURRENT_TIMESTAMP', 'DATABASE', 'DATABASES', 'DAY', 'DAY_HOUR', 'DAY_MINUTE',
         'DAY_SECOND', 'DEFAULT', 'DEFINER', 'DELAYED', 'DELETE', 'DESC', 'DESCRIBE', 'DETERMINISTIC', 'DISTINCT', 'DISTINCTROW', 'DIV',
-        'DO', 'DUMPFILE', 'DUPLICATE', 'DYNAMIC', 'ELSE', 'ENCLOSED', 'END', 'ENGINE', 'ENGINE_TYPE', 'ENGINES', 'ESCAPE', 'ESCAPED', 'EVENTS', 'EXEC', 
+        'DO', 'DUMPFILE', 'DUPLICATE', 'DYNAMIC', 'ELSE', 'ENCLOSED', 'END', 'ENGINE', 'ENGINE_TYPE', 'ENGINES', 'ESCAPE', 'ESCAPED', 'EVENTS', 'EXEC',
         'EXECUTE', 'EXISTS', 'EXPLAIN', 'EXTENDED', 'FAST', 'FIELDS', 'FILE', 'FIRST', 'FIXED', 'FLUSH', 'FOR', 'FORCE', 'FOREIGN', 'FULL', 'FULLTEXT',
         'FUNCTION', 'GLOBAL', 'GRANT', 'GRANTS', 'GROUP_CONCAT', 'HEAP', 'HIGH_PRIORITY', 'HOSTS', 'HOUR', 'HOUR_MINUTE',
         'HOUR_SECOND', 'IDENTIFIED', 'IF', 'IFNULL', 'IGNORE', 'IN', 'INDEX', 'INDEXES', 'INFILE', 'INSERT', 'INSERT_ID', 'INSERT_METHOD', 'INTERVAL',
@@ -121,6 +121,13 @@ class SqlFormatter
     // This affects the type of syntax highlighting
     // If not defined, it will be determined automatically
     public static $cli;
+
+    // Comments
+    public static $commentTokens = [
+        ['#'],
+        ['--'],
+        ['/*', '*/']
+    ];
 
     // For CLI syntax highlighting
     public static $cli_quote = "\x1b[34;1m";
@@ -214,38 +221,33 @@ class SqlFormatter
         }
 
         // Comment
-        if ($string[0] === '#' || (isset($string[1])&&($string[0]==='-'&&$string[1]==='-') || ($string[0]==='/'&&isset($string[1])&&$string[1]==='*'))) {
-            // Comment until end of line
-            if ($string[0] === '-' || $string[0] === '#') {
-                $last = strpos($string, "\n");
-                $type = self::TOKEN_TYPE_COMMENT;
-            } else { // Comment until closing comment tag or end of query
-                if (strpos($string, "*/", 2) !== false) {
-                    $last = strpos($string, "*/", 2) + 2;
-                } else {
-                    $last = strlen($string);
-                }
-                $type = self::TOKEN_TYPE_BLOCK_COMMENT;
+        $found = false;
+        foreach (self::$commentTokens as $comment) {
+            $start = $comment[0];
+            $end = isset($comment[1]) ? $comment[1] : "\n";
+            $found = substr($string, 0, strlen($start)) === $start;
+            if ($found) {
+                break;
             }
-
-            if ($last === false) {
-                $last = strlen($string);
-            }
-
+        }
+        if ($found) {
+            /** @var string $start */
+            /** @var string $end */
+            $endPos = strpos($string, $end, strlen($start));
+            $last = $endPos ?
+                ($endPos + ($end === "\n" ? 0 : strlen($end))) : strlen($string);
             return array(
                 self::TOKEN_VALUE => substr($string, 0, $last),
-                self::TOKEN_TYPE  => $type
+                self::TOKEN_TYPE  => $end === "\n" ? self::TOKEN_TYPE_COMMENT : self::TOKEN_TYPE_BLOCK_COMMENT
             );
         }
 
         // Quoted String
         if ($string[0]==='"' || $string[0]==='\'' || $string[0]==='`' || $string[0]==='[') {
-            $return = array(
+            return array(
                 self::TOKEN_TYPE => (($string[0]==='`' || $string[0]==='[')? self::TOKEN_TYPE_BACKTICK_QUOTE : self::TOKEN_TYPE_QUOTE),
                 self::TOKEN_VALUE => self::getQuotedString($string)
             );
-
-            return $return;
         }
 
         // User-defined Variable
@@ -254,7 +256,7 @@ class SqlFormatter
                 self::TOKEN_VALUE => null,
                 self::TOKEN_TYPE => self::TOKEN_TYPE_VARIABLE
             );
-            
+
             // If the variable name is quoted
             if ($string[1]==='"' || $string[1]==='\'' || $string[1]==='`') {
                 $ret[self::TOKEN_VALUE] = $string[0].self::getQuotedString(substr($string,1));
@@ -266,7 +268,7 @@ class SqlFormatter
                     $ret[self::TOKEN_VALUE] = $matches[1];
                 }
             }
-            
+
             if($ret[self::TOKEN_VALUE] !== null) return $ret;
         }
 
@@ -336,7 +338,7 @@ class SqlFormatter
     protected static function getQuotedString($string)
     {
         $ret = null;
-        
+
         // This checks for the following patterns:
         // 1. backtick quoted string using `` to escape
         // 2. square bracket quoted string (SQL Server) using ]] to escape
@@ -345,7 +347,7 @@ class SqlFormatter
         if ( preg_match('/^(((`[^`]*($|`))+)|((\[[^\]]*($|\]))(\][^\]]*($|\]))*)|(("[^"\\\\]*(?:\\\\.[^"\\\\]*)*("|$))+)|((\'[^\'\\\\]*(?:\\\\.[^\'\\\\]*)*(\'|$))+))/s', $string, $matches)) {
             $ret = $matches[1];
         }
-        
+
         return $ret;
     }
 
@@ -699,14 +701,14 @@ class SqlFormatter
             if ($token[self::TOKEN_VALUE] === '(' || $token[self::TOKEN_VALUE] === '.') {
                 $return = rtrim($return,' ');
             }
-            
+
             // If this is the "-" of a negative number, it shouldn't have a space after it
             if($token[self::TOKEN_VALUE] === '-' && isset($tokens[$i+1]) && $tokens[$i+1][self::TOKEN_TYPE] === self::TOKEN_TYPE_NUMBER && isset($tokens[$i-1])) {
                 $prev = $tokens[$i-1][self::TOKEN_TYPE];
                 if($prev !== self::TOKEN_TYPE_QUOTE && $prev !== self::TOKEN_TYPE_BACKTICK_QUOTE && $prev !== self::TOKEN_TYPE_WORD && $prev !== self::TOKEN_TYPE_NUMBER) {
                     $return = rtrim($return,' ');
                 }
-            } 
+            }
         }
 
         // If there are unmatched parentheses
